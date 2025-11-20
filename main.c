9975b96f69712619881cc24d22dd359f03d6be25
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-19 23:02:01 -0500

    Process, Node, and Queue functions defined. Processesss stored in Nodess, and Nodes in the Queue.

--- main.c
+++ main.c
@@ -3,3 +3,26 @@
 #include <string.h>
 
 #define MAX_PRIORITY 10
+
+typedef struct {
+    int pid;
+    int arrivalTime;
+    int burstTime;
+    int priority; 
+    int remainingTime;
+    
+    int waitingTime;
+    int turnaroundTime;
+    int completionTime;
+    int responseTime;
+} Process;
+
+typedef struct Node {
+    Process* data;      
+    struct Node* next;
+} Node;
+
+typedef struct {
+    Node* front;
+    Node* rear;
+} Queue;
