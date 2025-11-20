1215c14dea8faeb0a92ca4140e092d40893e0e36
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-19 23:12:10 -0500

    Create push and pop functions, which place a process at the abck of the queue and pop one to be sent to CPU, respectively

--- main.c
+++ main.c
@@ -33,3 +33,30 @@ Queue* createQueue() {
     q->rear = NULL;
     return q;
 }
+void push(Queue* q, Process* p) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = p;
+    newNode->next = NULL;
+
+    if (q->front == NULL) {
+        q->front = newNode;
+        q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+}
+Process* pop(Queue* q) {
+    if (q->front == NULL) return NULL;
+
+    Node* temp = q->front;
+    Process* p = temp->data;
+
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+
+    free(temp); 
+    return p;
+}
